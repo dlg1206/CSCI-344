476c178fb6a0750621d9a668ee05b5c76b488fce
Nickhunt2243 <72165125+Nickhunt2243@users.noreply.github.com>
2023-01-26 18:45:20 -0500

    Added the generic methods we will need for the tokenizer. Also added some of the parsing we still will have to parse through chars in the line

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -6,9 +6,43 @@ package provided;
  * @author 
  **/
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JottTokenizer {
+	// Receives a char and will return a TokenType based on Which type it falls under. We only do this
+	// If curr TokenType == Null
+	private TokenType getTokenType(char currChar) { return null;}
+
+	private void handleComment() { }
+
+	private void addCommaToken() { }
+
+	private void addLBracketToken() { }
+
+	private void addRBracketToken() { }
+
+	private void addLBraceToken() { }
+
+	private void addRBraceToken() { }
+
+	private void addSemiColonToken() { }
+
+	private void addColonToken() { }
+
+	private void addMathOpToken() { }
+
+	private void handleAssignmentToken() { }
+
+	private void handleRelOpToken() {	}
+
+	private void handleNumberToken() { }
+
+	private void handleIdKeywordToken() {	}
+
+	private void handleStringToken() {	}
 
 	/**
      * Takes in a filename and tokenizes that file into Tokens
@@ -17,6 +51,26 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
-		return null;
+			Scanner inputScan;
+			try {
+				File inputFile = new File(filename);
+				inputScan = new Scanner(inputFile);
+			} catch (FileNotFoundException fileNotFoundException) {
+				System.err.println(fileNotFoundException);
+				return null;
+			}
+
+			ArrayList<Token> tokenList = new ArrayList<>();
+			String currToken = "";
+			TokenType currTokenType = null;
+			String currLine;
+			while (inputScan.hasNextLine()) {
+				currLine = inputScan.nextLine();
+				// Loop through chars of line
+			}
+
+
+
+			return null;
 	}
 }
