d61380d1707ecdfc9811324c83cfb2368fd947f3
Nicholas R Hunt <72165125+Nickhunt2243@users.noreply.github.com>
2023-02-02 16:28:26 -0500

    Updating Tokenizer

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -13,7 +13,47 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JottTokenizer {
+	
+	private String currLine = "";
 
+
+	private static TokenType getTokenType(char currChar) {
+		
+		TokenType type = switch (currChar) {
+			case ',' -> return TokenType.COMMA;
+			case ']' -> return TokenType.L_BRACKET;
+			case '[' -> return TokenType.R_BRACKET;
+			case '}' -> return TokenType.L_BRACE;
+			case '{' -> return TokenType.R_BRACE;
+			case ';' -> return TokenType.SEMICOLON;
+			case ':' -> return TokenType.COLON;
+			// All math tokens are 'mathOp' tokens
+			case '/', '+', '-', '*' -> return TokenType.MATH_OP;
+			default -> null;
+		};
+		
+		if (type == null) {
+			if (currChar == '=') {
+				return TokenType.ASSIGN;
+			} else if (currChar == '<' || currChar == '>') {
+				return TokenType,REL_OP;
+			} else if (currChar == '.') {
+				return TokenType.NUMBER;
+			} else if (currChar >= '0' && currChar <= '9') {
+				return TokenType.NUMBER;
+			} else if (Character.toLowerCase(currChar) >= 'a'
+						&& Character.toLowerCase(currChar) <= 'z') {
+				return TokenType.ID_KEYWORD;
+			} else if (currChar == '!') {
+				return TokenType.REL_OP;
+			} else if (currChar == '"') {
+				return TokenType.STRING;
+			} else {
+				// throw error
+			}
+		}
+	}
+	
 	private static void handleComment() { }
 
 	private static int handleAssignmentToken() { return 0; }
@@ -26,7 +66,7 @@ public class JottTokenizer {
 
 	private static int handleIdKeywordToken() { return 0; }
 
-	private static int handleStringToken() { return 0;	}
+	private static int handleStringToken() { return 0; }
 
 	/**
      * Takes in a filename and tokenizes that file into Tokens
@@ -53,7 +93,7 @@ public class JottTokenizer {
 
 		// Parse file until EOF
 		while (inputScan.hasNextLine()) {
-			String currLine = inputScan.nextLine();
+			currLine = inputScan.nextLine();
 			lineNum++;
 
 			// Parse each character of the current line
@@ -61,49 +101,17 @@ public class JottTokenizer {
 				char currChar = currLine.charAt(i);
 				currTokenString.append(currChar);
 				if (currChar == '#') {
-					// break?
-					handleComment();
+					break;
 				}
 
 				// Check if simple token only if current token string is 1 character
-				if(currTokenString.length() == 1){
-					TokenType tokenType = switch (currChar) {
-						case ',' -> TokenType.COMMA;
-						case ']' -> TokenType.L_BRACKET;
-						case '[' -> TokenType.R_BRACKET;
-						case '}' -> TokenType.L_BRACE;
-						case '{' -> TokenType.R_BRACE;
-						case ';' -> TokenType.SEMICOLON;
-						case ':' -> TokenType.COLON;
-						// All math tokens are 'mathOp' tokens
-						case '/', '+', '-', '*' -> TokenType.MATH_OP;
-						default -> null;
-					};
+				TokenType tokenType = getTokenType(currChar);
 
-					// Add to token list if token was found
-					if(tokenType != null){
-						tokenList.add(new Token(currTokenString.toString(), fileName, lineNum, tokenType));
-						currTokenString = new StringBuilder();	// reset token string
-						continue;
-					}
-				}
-
-
-				if (currChar == '=') {
-//					i = handleAssignmentToken();
-				} else if (currChar == '<' || currChar == '>') {
-//					i = handleRelOpToken();
-				} else if (currChar == '.') {
-//					i = handleDecimal();
-				} else if (currChar >= '0' && currChar <= '9') {
-//					i = handleNumberToken();
-				} else if (Character.toLowerCase(currChar) >= 'a'
-							&& Character.toLowerCase(currChar) <= 'z') {
-//				  i = handleIdKeywordToken();
-				} else if (currChar == '!') {
-//					i = handleRelOpToken();
-				} else if (currChar == '"') {
-//					i = handleStringToken();
+				switch(tokenType) {
+					// Call method based on type	
+					case TokenType.STRING:
+						i = handleStringToken(i);
+						break;
 				}
 			}
 		}
