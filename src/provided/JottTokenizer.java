e2d393c95a986f2739fdb34cd2823626d51ec2ba
ZoeWheatcroft <zoe.wheatcroft@gmail.com>
2023-02-02 16:39:14 -0500

    Cleaned code w/ celeste :)
    Deleted the returns because they were giving errors, should still work. Made the curr line static so it can be used in static functions

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -13,36 +13,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JottTokenizer {
-	
-	private String currLine = "";
+
+	//line currently processing, allows you to look ahead because you can access the current line from token handling funcs
+	private static String currLine = "";
 
 
 	private static TokenType getTokenType(char currChar) {
-		
+
 		TokenType type = switch (currChar) {
-			case ',' -> return TokenType.COMMA;
-			case ']' -> return TokenType.L_BRACKET;
-			case '[' -> return TokenType.R_BRACKET;
-			case '}' -> return TokenType.L_BRACE;
-			case '{' -> return TokenType.R_BRACE;
-			case ';' -> return TokenType.SEMICOLON;
-			case ':' -> return TokenType.COLON;
+			case ',' ->  TokenType.COMMA;
+			case ']' ->  TokenType.L_BRACKET;
+			case '[' ->  TokenType.R_BRACKET;
+			case '}' ->  TokenType.L_BRACE;
+			case '{' ->  TokenType.R_BRACE;
+			case ';' ->  TokenType.SEMICOLON;
+			case ':' ->  TokenType.COLON;
 			// All math tokens are 'mathOp' tokens
-			case '/', '+', '-', '*' -> return TokenType.MATH_OP;
+			case '/', '+', '-', '*' ->  TokenType.MATH_OP;
 			default -> null;
 		};
-		
+
 		if (type == null) {
 			if (currChar == '=') {
 				return TokenType.ASSIGN;
 			} else if (currChar == '<' || currChar == '>') {
-				return TokenType,REL_OP;
+				return TokenType.REL_OP;
 			} else if (currChar == '.') {
 				return TokenType.NUMBER;
 			} else if (currChar >= '0' && currChar <= '9') {
 				return TokenType.NUMBER;
 			} else if (Character.toLowerCase(currChar) >= 'a'
-						&& Character.toLowerCase(currChar) <= 'z') {
+					&& Character.toLowerCase(currChar) <= 'z') {
 				return TokenType.ID_KEYWORD;
 			} else if (currChar == '!') {
 				return TokenType.REL_OP;
@@ -52,29 +53,47 @@ public class JottTokenizer {
 				// throw error
 			}
 		}
+		return null; //shouldn't get here
 	}
-	
-	private static void handleComment() { }
 
-	private static int handleAssignmentToken() { return 0; }
+	//TOKEN HANDLER FUNCTIONS
+	//each function should return an int of how many characters it looked ahead
+	//gl lads
+	private static void handleComment() {
+	}
 
-	private static int handleRelOpToken() { return 0;	}
+	private static int handleAssignmentToken() {
+		return 0;
+	}
 
-	private static int handleDecimal() { return 0; }
+	private static int handleRelOpToken() {
+		return 0;
+	}
 
-	private static int handleNumberToken() { return 0; }
+	private static int handleDecimal() {
+		return 0;
+	}
 
-	private static int handleIdKeywordToken() { return 0; }
+	private static int handleNumberToken() {
+		return 0;
+	}
 
-	private static int handleStringToken() { return 0; }
+	private static int handleIdKeywordToken() {
+		return 0;
+	}
+
+	private static int handleStringToken() {
+		return 0;
+	}
 
 	/**
-     * Takes in a filename and tokenizes that file into Tokens
-     * based on the rules of the Jott Language
-     * @param fileName the name of the file to tokenize; can be relative or absolute path
-     * @return an ArrayList of Jott Tokens
-     */
-    public static ArrayList<Token> tokenize(String fileName){
+	 * Takes in a filename and tokenizes that file into Tokens
+	 * based on the rules of the Jott Language
+	 *
+	 * @param fileName the name of the file to tokenize; can be relative or absolute path
+	 * @return an ArrayList of Jott Tokens
+	 */
+	public static ArrayList<Token> tokenize(String fileName) {
 
 		// Attempt to read file
 		Scanner inputScan;
@@ -89,7 +108,7 @@ public class JottTokenizer {
 		// init tokenization variables
 		ArrayList<Token> tokenList = new ArrayList<>();
 		StringBuilder currTokenString = new StringBuilder();
-		int lineNum = 0;	// Needed for Token
+		int lineNum = 0;    // Needed for Token
 
 		// Parse file until EOF
 		while (inputScan.hasNextLine()) {
@@ -107,11 +126,10 @@ public class JottTokenizer {
 				// Check if simple token only if current token string is 1 character
 				TokenType tokenType = getTokenType(currChar);
 
-				switch(tokenType) {
+				//call your token handling function in here
+				switch (tokenType) {
 					// Call method based on type	
-					case TokenType.STRING:
-						i = handleStringToken(i);
-						break;
+
 				}
 			}
 		}
@@ -119,3 +137,5 @@ public class JottTokenizer {
 		// return list of tokens found
 		return tokenList;
 	}
+
+}
