c3393608c43b544f9da21395d7d64ad7433a93e2
Derek Garcia <dlg1206@g.rit.edu>
2023-02-04 18:45:17 -0500

    basic token strings, whitespace, and comments

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -1,90 +1,17 @@
-
 package provided;
-
-/**
- * This class is responsible for tokenizing Jott code.
- * 
- * @author Derek Garcia
- **/
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class is responsible for tokenizing Jott code.
+ *
+ * @author Derek Garcia
+ **/
+
 public class JottTokenizer {
-
-	//line currently processing, allows you to look ahead because you can access the current line from token handling funcs
-	private static String currLine = "";
-
-
-	private static TokenType getTokenType(char currChar) {
-
-		TokenType type = switch (currChar) {
-			case ',' ->  TokenType.COMMA;
-			case ']' ->  TokenType.L_BRACKET;
-			case '[' ->  TokenType.R_BRACKET;
-			case '}' ->  TokenType.L_BRACE;
-			case '{' ->  TokenType.R_BRACE;
-			case ';' ->  TokenType.SEMICOLON;
-			case ':' ->  TokenType.COLON;
-			// All math tokens are 'mathOp' tokens
-			case '/', '+', '-', '*' ->  TokenType.MATH_OP;
-			default -> null;
-		};
-
-		if (type == null) {
-			if (currChar == '=') {
-				return TokenType.ASSIGN;
-			} else if (currChar == '<' || currChar == '>') {
-				return TokenType.REL_OP;
-			} else if (currChar == '.') {
-				return TokenType.NUMBER;
-			} else if (currChar >= '0' && currChar <= '9') {
-				return TokenType.NUMBER;
-			} else if (Character.toLowerCase(currChar) >= 'a'
-					&& Character.toLowerCase(currChar) <= 'z') {
-				return TokenType.ID_KEYWORD;
-			} else if (currChar == '!') {
-				return TokenType.REL_OP;
-			} else if (currChar == '"') {
-				return TokenType.STRING;
-			} else {
-				// throw error
-			}
-		}
-		return null; //shouldn't get here
-	}
-
-	//TOKEN HANDLER FUNCTIONS
-	//each function should return an int of how many characters it looked ahead
-	//gl lads
-	private static void handleComment() {
-	}
-
-	private static int handleAssignmentToken() {
-		return 0;
-	}
-
-	private static int handleRelOpToken() {
-		return 0;
-	}
-
-	private static int handleDecimal() {
-		return 0;
-	}
-
-	private static int handleNumberToken() {
-		return 0;
-	}
-
-	private static int handleIdKeywordToken() {
-		return 0;
-	}
-
-	private static int handleStringToken() {
-		return 0;
-	}
 
 	/**
 	 * Takes in a filename and tokenizes that file into Tokens
@@ -107,30 +34,49 @@ public class JottTokenizer {
 
 		// init tokenization variables
 		ArrayList<Token> tokenList = new ArrayList<>();
-		StringBuilder currTokenString = new StringBuilder();
+		StringBuilder tokenString = new StringBuilder();
 		int lineNum = 0;    // Needed for Token
 
 		// Parse file until EOF
 		while (inputScan.hasNextLine()) {
-			currLine = inputScan.nextLine();
+			String currLine = inputScan.nextLine();
 			lineNum++;
 
 			// Parse each character of the current line
 			for (int i = 0; i < currLine.length(); i++) {
 				char currChar = currLine.charAt(i);
-				currTokenString.append(currChar);
-				if (currChar == '#') {
+				// skip if whitespace
+				if(currChar == ' ')
+					continue;
+
+				// if a comment, trash rest of line
+				if(currChar == '#')
 					break;
+
+				// else start building token
+				tokenString.append(currChar);
+
+				// test if basic type
+				TokenType type = switch (currChar) {
+					case ',' ->  TokenType.COMMA;
+					case ']' ->  TokenType.L_BRACKET;
+					case '[' ->  TokenType.R_BRACKET;
+					case '}' ->  TokenType.L_BRACE;
+					case '{' ->  TokenType.R_BRACE;
+					case ';' ->  TokenType.SEMICOLON;
+					case ':' ->  TokenType.COLON;
+					// All math tokens are 'mathOp' tokens
+					case '/', '+', '-', '*' ->  TokenType.MATH_OP;
+					default -> null;
+				};
+
+				// If basic type, add to token list
+				if(type != null){
+					tokenList.add(new Token(tokenString.toString(), fileName, lineNum, type));
+					tokenString = new StringBuilder();	// reset token sting
+					continue;
 				}
 
-				// Check if simple token only if current token string is 1 character
-				TokenType tokenType = getTokenType(currChar);
-
-				//call your token handling function in here
-				switch (tokenType) {
-					// Call method based on type	
-
-				}
 			}
 		}
 
