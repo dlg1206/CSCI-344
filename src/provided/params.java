e3ee43a5d95713f62d7e49de50704dd11b385f38
dlg1206 <dlg1206@g.rit.edu>
2023-03-05 16:09:46 -0500

    initial params and params tail

--- src/provided/params.java
+++ src/provided/params.java
@@ -8,34 +8,39 @@ import java.util.ArrayList;
 
 public class params implements JottTree{
 
-    /*
-    above is just while != ]
-        parseParam tokens
-     */
-    static params parseParams(ArrayList<Token> tokens) throws Exception {
-        // < params > -> < expr > < params_t >
+
+    // Temp testing funct
+    public static void main(String[] args) throws Exception {
+        ArrayList<Token> params = new ArrayList<>();
+        params.add(new Token("[", "foo", 0, TokenType.L_BRACKET));
+        params.add(new Token("5", "foo", 0, TokenType.NUMBER));
+        params.add(new Token(",", "foo", 0, TokenType.COMMA));
+        params.add(new Token("7", "foo", 0, TokenType.NUMBER));
+        params.add(new Token("]", "foo", 0, TokenType.R_BRACKET));
+
+        parseParams(params);
+
+    }
+    public static ArrayList<Token> parseParams(ArrayList<Token> tokens) throws Exception {
         try {
-            for(;;){
+            tokens.remove(0);   // pop "["
 
-                Token curToken = tokens.remove(0);
+            tokens = expr.parseExpr(tokens);
+            tokens = params_t.parseParams_t(tokens);
 
-                if(curToken.getToken().equals("]"))
-                    break;
-                params_t.parseParams_t(tokens);
+            tokens.remove(0);   // pop "]"
+            return tokens;
 
-            }
-
-
-//            return ;
         } catch (IndexOutOfBoundsException e){
             /*
+            todo err msgs
             Syntax Error:
             <Message>
             <filename>:<line_number>
             */
             throw new Exception("Failed to parse params");
         }
-        return null;
+
     }
 
     @Override
--- src/provided/params_t.java
+++ src/provided/params_t.java
@@ -6,8 +6,55 @@ import java.util.ArrayList;
  * @author Derek Garcia
  **/
 
-public interface params_t {
-    static params_t parseParams_t(ArrayList<Token> tokens){
+public class params_t implements JottTree{
+    public static ArrayList<Token> parseParams_t(ArrayList<Token> tokens){
+        try {
+            // base case
+            if(tokens.get(0).getToken().equals("]"))
+                return tokens;
+
+            tokens.remove(0);   // pop ","
+
+            tokens = expr.parseExpr(tokens);
+            tokens = params_t.parseParams_t(tokens);
+
+            return tokens;
+
+        } catch (IndexOutOfBoundsException e){
+            /*
+            Syntax Error:
+            <Message>
+            <filename>:<line_number>
+            */
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return null;
+    }
+
+    @Override
+    public String convertToJott() {
+        return null;
+    }
+
+    @Override
+    public String convertToJava(String className) {
+        return null;
+    }
+
+    @Override
+    public String convertToC() {
+        return null;
+    }
+
+    @Override
+    public String convertToPython() {
+        return null;
+    }
+
+    @Override
+    public boolean validateTree() {
+        return false;
     }
 }
