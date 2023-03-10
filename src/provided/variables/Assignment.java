4a499451ccdad27cf2d765e9d95bbed0bec96c49
dlg1206 <dlg1206@g.rit.edu>
2023-03-10 10:32:51 -0500

    implemented asmt with end stmt and parsingerror

--- src/provided/variables/Assignment.java
+++ src/provided/variables/Assignment.java
@@ -1,13 +1,10 @@
 package provided.variables;
 
-import provided.JottTree;
+import provided.ParsingError;
 import provided.Token;
 import provided.variables.basics.Id;
 import provided.variables.basics.Type;
 import provided.variables.expr.Expression;
-
-
-import provided.variables.Stmt;
 
 import java.util.ArrayList;
 
@@ -22,8 +19,7 @@ public class Assignment extends Stmt {
     private final Type type;
      private final Id id;
      private final Expression expr;
-    // todo add w/ future implementation
-    // private final EndStatement end_statement
+     private final EndStmt end_statement;
 
 
     /**
@@ -31,13 +27,11 @@ public class Assignment extends Stmt {
      *
      * @param type variable type
      */
-    // todo replace with correct
-    // private Assignment(Type type, Id id, Expression expr, EndStatement end_statement)
-    private Assignment(Type type, Id id, Expression expr) {
+     private Assignment(Type type, Id id, Expression expr, EndStmt end_statement) {
         this.type = type;
-         this.id = id;
-         this.expr = expr;
-        // this.end_statement = end_statement;
+        this.id = id;
+        this.expr = expr;
+        this.end_statement = end_statement;
     }
 
     /**
@@ -47,13 +41,22 @@ public class Assignment extends Stmt {
      * @return new asmt object
      */
     public static Assignment parseAsmt(ArrayList<Token> tokens) {
-        Type type = Type.getType(tokens.remove(0));
-         Id id = Id.createId(tokens);
-        tokens.remove(0);   // pop '='
-         Expression expr = Expression.createExpression(tokens);
-        // EndStatement end_statement = EndStatement.parseEnd_statement(tokens);
-        // return new Assignment(type, id, expr, end_statement);
-        return new Assignment(type, id, expr);
+        try{
+            Type type = Type.getType(tokens.remove(0));
+            Id id = Id.createId(tokens);
+
+            // Validate
+            if(!tokens.get(0).getToken().equals("="))
+                throw new ParsingError("Syntax Error", "=", tokens.get(0));
+            tokens.remove(0);   // pop '='
+
+            Expression expr = Expression.createExpression(tokens);
+            EndStmt end_statement = EndStmt.parseEndStmt(tokens);
+
+            return new Assignment(type, id, expr, end_statement);
+        } catch (ParsingError e){
+            return null;
+        }
     }
 
     /**
@@ -63,9 +66,8 @@ public class Assignment extends Stmt {
      */
     @Override
     public String convertToJott() {
-        // return this.type.convertToJott() + " " + this.id.convertToJott() + " = "
-        // + this.expr.convertToJott() + this.end_statement.convertToJott();
-        return null;
+         return this.type.convertToJott() + " " + this.id.convertToJott() + " = "
+                 + this.expr.convertToJott() + this.end_statement.convertToJott();
     }
 
     @Override
